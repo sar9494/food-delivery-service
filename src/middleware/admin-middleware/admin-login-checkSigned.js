3761492425caaf09943c5ac73a09034a7d63a336
sar9494 <batbayarsaruul0@gmail.com>
2025-02-21 20:32:23 +0800

    sdf

--- src/middleware/admin-middleware/admin-login-checkSigned.js
+++ src/middleware/admin-middleware/admin-login-checkSigned.js
@@ -1,14 +1,18 @@
 import { Admin } from "../../schemas/admin.js"
-export const checkSigned =async (req,res)=>{
+export const checkSigned = async (req, res) => {
     try {
-        const {email,password}=req.body
-        
-    const isSigned = await Admin.findOne({email});
-    console.log(isSigned);
-    res.send(isSigned).status(200)
+        const { email, password } = req.body
+        const isSigned = await Admin.findOne({ email });
+        if(isSigned.password===password){
+        res.send({
+            su
+        }).status(200)
+        }
+        console.log(isSigned);
+        res.send(isSigned).status(200)
     } catch (error) {
         console.log(error);
-        
+
     }
-    
+//df
 }
