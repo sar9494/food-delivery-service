cc1ef7e8831c65be1bf3ad4011c9c9c81e7a5421
sar9494 <batbayarsaruul0@gmail.com>
2025-03-11 18:11:21 +0800

    asf

--- src/controller/foodControllers/getFoods.js
+++ src/controller/foodControllers/getFoods.js
@@ -2,7 +2,7 @@ import { FoodModel } from "../../models/foodSchema.js";
 export const getFoods = async (req, res) => {
   try {
     const allFoods = await FoodModel.find();
-    res.starus(200).send(allFoods);
+    res.status(200).send(allFoods);
   } catch (error) {
     return res.status(500).send({
       success: false,
--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ const app = express();
 const port = 5000;
 app.use(express.json());
 app.use(cors());
+
 app.use("/user", userRouter);
 app.use("/", categoryRouter);
 app.use("/", foodRouter);
